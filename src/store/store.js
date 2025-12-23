cd8578e87e75cd8f317e62ab58de0edb870a76e4
Ishrz <ishrzz66@gmail.com>
2025-12-23 16:20:48 +0530

    added root reducer in store, for scalability and clean code

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, compose, createStore} from 'redux'
-import { weatherReducer } from './reducers/weatherReducer';
+// import { weatherReducer } from './reducers/weatherReducer';
 import {thunk} from 'redux-thunk'
+import rootReducer from './reducers';
 
 const reduxDevtools= window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store=createStore(
-    weatherReducer,
+    rootReducer,
     compose(
         applyMiddleware(thunk),
         // reduxDevtools
